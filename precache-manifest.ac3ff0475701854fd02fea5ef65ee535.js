8e51eabfb0dd54e983e11ca6d134b3f254abb1eb
carlos <aldacor2011@hotmail.com>
2019-11-12 23:45:16 -0300

    Updates

---
+++ precache-manifest.ac3ff0475701854fd02fea5ef65ee535.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7165e10dc59d11ca033af06cde7a5e80",
+    "url": "/presentation-portfolio/index.html"
+  },
+  {
+    "revision": "06a7821473a1c95a1f04",
+    "url": "/presentation-portfolio/static/css/main.d1b945db.chunk.css"
+  },
+  {
+    "revision": "544cd93fcbe5c25000d0",
+    "url": "/presentation-portfolio/static/js/2.6d02a3e9.chunk.js"
+  },
+  {
+    "revision": "06a7821473a1c95a1f04",
+    "url": "/presentation-portfolio/static/js/main.5b8f91e6.chunk.js"
+  },
+  {
+    "revision": "47e8732e4df6ec63e115",
+    "url": "/presentation-portfolio/static/js/runtime-main.2ab20ce1.js"
+  },
+  {
+    "revision": "15bfee504a218573b5d771adcf0f328d",
+    "url": "/presentation-portfolio/static/media/code.15bfee50.mp4"
+  }
+]);
